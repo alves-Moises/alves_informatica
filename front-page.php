c7a93f29e8a1a01e213049c457f0dedd60227536
alves-Moises <moisesinho_ra@hotmail.com>
2023-03-12 16:32:30 -0300

    update front-age: cleancode

--- front-page.php
+++ front-page.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
+
 <?php get_template_part("template-parts/navigation/navbar"); ?>
 <?php get_template_part("template-parts/front-page/header"); ?>
 <?php get_template_part("template-parts/front-page/portifolio"); ?>
 <?php get_template_part("template-parts/front-page/about"); ?>
 <?php get_template_part("template-parts/front-page/contact"); ?>
 <?php get_template_part("template-parts/front-page/modal"); ?>
-
 
 <?php get_footer(); ?>
