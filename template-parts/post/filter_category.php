a7cdc146798cf207a51730ed2b8128f8459781b8
alves-Moises <moisesinho_ra@hotmail.com>
2023-04-02 22:01:54 -0300

    new: filter_by_category

--- template-parts/post/filter_category.php
+++ template-parts/post/filter_category.php
@@ -1,15 +1,33 @@
 
-<?php
-  $category_detail=get_the_category('0');//$post->ID
-  foreach($category_detail as $cd): if($cd == "portifolio_servicos"): 
+
+
+
+<?php 
+    $posts = get_posts($category = "portifolio_servicos");
 ?>
-    <!-- echo $cd->cat_name;
-    echo get_the_title();
-    echo the_content();
-    echo "<h1> teste </h1>"; -->
 
-<div>
-  <?php echo $cd->cat_name ?>
-</div>
+<?php foreach($posts as $post):?>
 
-<?php endif; endforeach; ?>
+<?php if(get_the_category()[0]->name == "portifolio_servicos"): ?>
+    <div class="col-md-6 col-lg-4 mb-5">
+
+    <div class="card card-" style="width: 18rem;">
+        <?php echo get_the_post_thumbnail($post->id, 'large', ['class' => 'img-fluid single-image', 'alt' => 'single image']); ?>
+        <div class="card-body">
+            <h5 class="card-title text-center"><?php echo $post->post_title ?></h5>
+            <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+            
+            <a href="#" class="btn btn-primary">Leia mais</a>
+            <a hered="#" class="btn btn-secondary">Entre em contato!</a>
+            </div>
+        </div>
+    </div>
+
+
+<?php endif; endforeach;?>
+
+
+asdasd
+
+
+<?php //endwhile; endif; ?>
