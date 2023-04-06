c336af76b3a0414b7e2b33cc3030a515dba5711d
alves-Moises <moisesinho_ra@hotmail.com>
2023-04-06 00:13:14 -0300

    update card: automation message and layout

--- template-parts/post/filter_category.php
+++ template-parts/post/filter_category.php
@@ -7,7 +7,7 @@
 <?php if(get_the_category()[0]->name == "portifolio_servicos"): ?>
 
     <div class="col-md-6 col-lg-4 mb-5">
-        <div class="card" style="width: 18rem;">
+        <div class="card card-servicos" style="width: 18rem;">
 
             <?php 
                 echo get_the_post_thumbnail(
@@ -21,12 +21,28 @@
             ?>
 
             <div class="card-body">
-                <h5 class="card-title text-center">
-                    <?php echo $post->post_title ?>
-                </h5>
-               
-                <a href="#" class="btn btn-primary">Leia mais</a>
-                <a hered="#" class="btn btn-secondary">Entre em contato!</a>
+                <div class="card-servicos-vertical-center">
+
+                    <h5 class="card-title text-center card-servicos-title">
+                        <?php echo $post->post_title ?>
+                    </h5>
+                </div>
+
+                <div class="btn-servicos">
+
+                    <a href="#" class="btn btn-primary">
+                        Leia mais
+                    </a>
+                </div>
+                
+                <div class="btn-servicos">
+                    <a 
+                        href="https://wa.me/5522999669509/?text=<?= get_post_custom_values("msg", $id)[0] ?>" 
+                        class="btn btn-secondary"
+                    >
+                        Entre em contato!
+                    </a>
+                </div>
             </div>
         </div>
     </div>
