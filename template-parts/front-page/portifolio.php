7c29ce98a59a9ba1db0793e94799acbc4ffcb848
alves-Moises <moisesinho_ra@hotmail.com>
2023-10-12 19:19:52 -0300

    fix cards portifolio

--- template-parts/front-page/portifolio.php
+++ template-parts/front-page/portifolio.php
@@ -8,15 +8,17 @@
             <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
             <div class="divider-custom-line"></div>
         </div>
+
         <!-- Portfolio Grid Items-->
         <div class="row justify-content-center">
             <!-- Portfolio Item 1-->
-            <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
-                <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                    <div class="col-md-6 col-lg-4 mb-5">
+            <div class="col-md-6 col-lg-4 mb-5">
+                <div class="portfolio-item mx-auto card-portfoilio" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                         <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
                     </div>
-                    <img class="img-fluid" src="<?= THEMEROOT ?>/assets/img/servicos/1_informatica.jpg" alt="Informática">
+                    <img class="img-fluid" src="<?= THEMEROOT ?>/assets/img/servicos/1_informatica.jpg" alt="Informatica">
+                    <p>niformatica</p>
                 </div>
             </div>
             <!-- Portfolio Item 2-->
