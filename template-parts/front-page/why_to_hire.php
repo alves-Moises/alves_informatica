edff3a0f6fc1098fcb359c33d828781801fde8bd
alves-Moises <moisesinho_ra@hotmail.com>
2023-11-27 19:29:05 -0300

    new section: why_to_hire

--- template-parts/front-page/why_to_hire.php
+++ template-parts/front-page/why_to_hire.php
@@ -5,14 +5,101 @@
             <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0 subtitle">
                 Por que contratar nossos servi&ccedil;os?
             </h2>
-            <div class="hire-cards justify-content-center">
+            <div class="divider-custom">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-line"></div>
+            </div>
+            <div class="hire-cards d-flex flex-nowrap justify-content-between">
                 
-                <div class="card hire_card text-center mt-1">
-                    <h2><i class="fa-solid fa-circle-check fa-2xl" style="color: #5c5c5c;"></i></h2>
+                <div class="card hire_card text-center m-2 col-lg-3 col-md-5">
+                    <h2>
+                        <i class="fa-solid fa-circle-check fa-2xl i-vantagens"></i>
+                    </h2>
                     <h4>
                         Servi&ccedil;os de qualdiade
                     </h4>
+                    <p class="text-left">
+                        Os nossos serviços excelentes são a sua melhor escolha para o seu sucesso.
+                        Com profissionais qualificados e comprometidos, nós garantimos a sua satisfação, 
+                        com serviços de alto padrão e seguindo padrões de excelência e ética.
+                    </p>
                 </div>
+
+                <div class="card hire_card text-center m-2 col-lg-3 col-md-5">
+                    <h2>
+                    <i class="fa-solid fa-hand-holding-dollar fa-2xl i-vantagens"></i>
+                    </h2>
+                    <h4>
+                        Preco justo
+                    </h4>
+                    <p class="text-left">
+                        Nós oferecemos um serviço de informática com um preço justo e competitivo, 
+                        sem cobrar taxas abusivas ou esconder custos adicionais. 
+                        Você paga pelo que precisa e recebe o que contratou.
+                    </p>
+                </div>
+
+                <div class="card hire_card text-center m-2 col-lg-3 col-md-5">
+                    <h2>
+                        <i class="fa-solid fa-hand-holding-dollar fa-2xl i-vantagens"></i>
+                    </h2>
+                    <h4>
+                        Atendiemnto personalizado
+                    </h4>
+                    <p class="text-left">
+                        Nós entendemos que cada cliente tem uma necessidade diferente e uma preferência pessoal. 
+                        Por isso, nós oferecemos um atendimento personalizado, ouvindo as suas demandas e 
+                        buscando a melhor solução para o seu problema.
+                    </p>
+                </div>
+
+                <div class="card hire_card text-center m-2 col-lg-3 col-md-5">
+                    <h2>
+                        <i class="fa-solid fa-hand-holding-dollar fa-2xl i-vantagens"></i>
+                    </h2>
+                    <h4>
+                        Experiência e qualificação
+                    </h4>
+                    <p class="text-left">
+                        Nós temos uma equipe de profissionais experientes e qualificados, 
+                        que estão sempre se atualizando e se aperfeiçoando nas novas tecnologias e tendências do mercado. 
+                        Nós garantimos um serviço de informática de alto nível e com resultados satisfatórios.
+
+                    </p>
+                </div>
+
+                <div class="card hire_card text-center m-2 col-lg-3 col-md-5">
+                    <h2>
+                        <i class="fa-solid fa-hand-holding-dollar fa-2xl i-vantagens"></i>
+                    </h2>
+                    <h4>
+                        Garantia e suporte
+                    </h4>
+                    <p class="text-left">
+                        Nós não abandonamos os nossos clientes após a entrega do serviço. 
+                        Nós oferecemos uma garantia de qualidade e um suporte técnico eficiente, 
+                        que está sempre à disposição para resolver qualquer dúvida ou problema que possa surgir.
+                    </p>
+                </div>
+
+                <div class="card hire_card text-center m-2 col-lg-3 col-md-5">
+                    <h2>
+                        <i class="fa-solid fa-hand-holding-dollar fa-2xl i-vantagens"></i>
+                    </h2>
+                    <h4>
+                        Compromisso e responsabilidade
+                    </h4>
+                    <p class="text-left">
+                        Nós levamos o nosso trabalho a sério e respeitamos os prazos e as expectativas dos nossos clientes. 
+                        Nós nos comprometemos a entregar um serviço de informática de excelência e com responsabilidade.
+                    </p>
+                </div>
+
+
+
+
+
             </div>
         </div>
     </div>
