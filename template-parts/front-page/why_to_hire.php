efa70c1e71455de09e42c2c0b8de80c21bd71947
alves-Moises <moisesinho_ra@hotmail.com>
2023-11-27 00:11:12 -0300

    new: why_to_hire temlate part

---
+++ template-parts/front-page/why_to_hire.php
@@ -0,0 +1,19 @@
+<div class="page-section">
+
+    <div class="container">
+        <div class="row mt-2 why-hire">
+            <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0 subtitle">
+                Por que contratar nossos servi&ccedil;os?
+            </h2>
+            <div class="hire-cards justify-content-center">
+                
+                <div class="card hire_card text-center mt-1">
+                    <h2><i class="fa-solid fa-circle-check fa-2xl" style="color: #5c5c5c;"></i></h2>
+                    <h4>
+                        Servi&ccedil;os de qualdiade
+                    </h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
