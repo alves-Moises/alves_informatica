628cc7f474d9ba246a2b8a77761c68b2c4ad9c0d
alves-Moises <moisesinho_ra@hotmail.com>
2023-12-05 22:18:28 -0300

    new: adress-section temmplate-part

---
+++ template-parts/front-page/adress-section/adress-section.php
@@ -0,0 +1,13 @@
+<!-- Adress Section Form-->
+
+<div class="row justify-content-center adress-row">
+
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d232.47245799314325!2d-41.88595706731533!3d-21.209657805439605!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xbc61ffcb93faab%3A0xeeef21c8a8aed616!2sAlves%20Inform%C3%A1tica!5e0!3m2!1sen!2sbr!4v1701815223417!5m2!1sen!2sbr" 
+        width="600" height="450" 
+        style="border:0;" 
+        allowfullscreen=""
+        loading="lazy" 
+        referrerpolicy="no-referrer-when-downgrade"
+    >
+    </iframe>
+</div>
