53ea53f096fed3ed2a6eac801c6f165b81490812
alves-Moises <moisesinho_ra@hotmail.com>
2023-12-09 01:00:29 -0300

    update: portifolio

--- template-parts/front-page/portifolio.php
+++ template-parts/front-page/portifolio.php
@@ -4,35 +4,45 @@
         <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Meus Serviços</h2>
         <!-- Icon Divider-->
         <div class="divider-custom">
-            <div class="divider-custom-line"></div>
-            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-            <div class="divider-custom-line"></div>
+            <div class="divider-custom-line i-symbols-line-dark"></div>
+            <div class="divider-custom-icon"><i class="fa-solid fa-screwdriver-wrench "></i></div>
+            <div class="divider-custom-line i-symbols-line-dark"></div>
         </div>
 
         <!-- Portfolio Grid Items-->
         <div class="row justify-content-center">
+
             <!-- Portfolio Item 1-->
             <div class="col-md-6 col-lg-4 mb-5">
-                <div class="portfolio-item mx-auto card-portfoilio" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                <div class="portfolio-item mx-auto div-card" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                         <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
                     </div>
                     <img class="img-fluid" src="<?= THEMEROOT ?>/assets/img/servicos/1_informatica.jpg" alt="Informatica">
-                    <div class="text-center">
-                        <p>Informática</p>
+                    <div class="text-center ">
+                        <h5 class="card-title">
+                            Informática
+                        </h5>
                     </div>
                         
                 </div>
             </div>
+
             <!-- Portfolio Item 2-->
             <div class="col-md-6 col-lg-4 mb-5">
-                <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
+                <div class="portfolio-item mx-auto div-card" data-bs-toggle="modal" data-bs-target="#portfolioModal2">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                         <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
                     </div>
                     <img class="img-fluid" src="<?= THEMEROOT ?>/assets/img/servicos/2_smartphone.jpg" alt="Smartphone">
+                    <div class="text-center ">
+                        <h5 class="card-title">
+                            Smartphones
+                        </h5>
+                    </div>
                 </div>
             </div>
+
             <!-- Portfolio Item 3-->
             <div class="col-md-6 col-lg-4 mb-5">
                 <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal3">
