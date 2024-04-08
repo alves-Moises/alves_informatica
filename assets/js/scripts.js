e1d3385e099c03ad49a2a121631d3f445998bf7c
alves-Moises <moisesinho_ra@hotmail.com>
2024-04-08 00:19:13 -0300

    update script copyright

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,6 @@
 /*!
-* Start Bootstrap - Freelancer v7.0.6 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+* Copyright 2024 - Alves informatica
+* Licensed under MIT (https://github.com/alves-Moises)
 */
 //
 // Scripts
