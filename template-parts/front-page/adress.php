b4e5e854e16213fa73f35d3cac17ddae5af048b8
alves-Moises <moisesinho_ra@hotmail.com>
2024-04-15 20:20:37 -0300

    cleancode

--- template-parts/front-page/adress.php
+++ template-parts/front-page/adress.php
@@ -1,20 +1,20 @@
 <!-- Adress Section-->
 <section class="page-section adress-section" id="adress">
-            <div class="container">
-                <!-- Adress Section Heading-->
-                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0 adress-title">Como chegar</h2>
-                
-                <!-- Icon Divider-->
-                <div class="divider-custom">
-                    <div class="divider-custom-line i-symbols-line"></div>
-                    <div class="divider-custom-icon"><i class="fas fa-location-crosshairs i-symbols"></i></div>
-                    <div class="divider-custom-line i-symbols-line"></div>
-                </div>
+    <div class="container">
+        <!-- Adress Section Heading-->
+        <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0 adress-title">Como chegar</h2>
+        
+        <!-- Icon Divider-->
+        <div class="divider-custom">
+            <div class="divider-custom-line i-symbols-line"></div>
+            <div class="divider-custom-icon"><i class="fas fa-location-crosshairs i-symbols"></i></div>
+            <div class="divider-custom-line i-symbols-line"></div>
+        </div>
 
-                <!-- Adress Section Form-->
-                <?php 
-                    get_template_part("template-parts/front-page/adress-section/adress-section"); 
-                ?>
+        <!-- Adress Section Form-->
+        <?php 
+            get_template_part("template-parts/front-page/adress-section/adress-section"); 
+        ?>
 
-            </div>
-        </section>
+    </div>
+</section>
