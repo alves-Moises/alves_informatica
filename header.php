8a08f04bc0eeaf9b9031254571ec473e24a80142
alves-Moises <moisesinho_ra@hotmail.com>
2025-10-20 01:20:48 -0300

    update header: cleancode

--- header.php
+++ header.php
@@ -1,13 +1,19 @@
 <!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
+
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	
+
 	<link rel="icon" href="<?= THEMEROOT ?>/assets/img/logo/microchip-solid.svg" syzes="any" type="image/svg+xml">
 
-	<title><?php echo is_front_page() ? '' : wp_title('', false).' | '; bloginfo('name'); ?></title>
+	<title>
+		<?php 
+			echo is_front_page() ? '' : wp_title('', false) . ' | ';
+			bloginfo('name'); 
+			?>
+	</title>
 
 	<!-- Favicon-->
 	<link rel="icon" type="image/x-icon" href="<?= THEMEROOT ?>/assets/favicon.ico">
@@ -16,14 +22,15 @@
 	<!-- Google fonts-->
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-	
+
 	<?php wp_head(); ?>
 
 </head>
+
 <body <?php body_class() ?> id="page-top">
 
 
-<?php 
-if ( ! (is_front_page() || is_home()) ) 
-	get_template_part('template-parts/navigation/navbar');
-?>
+	<?php
+	if (!(is_front_page() || is_home()))
+		get_template_part('template-parts/navigation/navbar');
+	?>
