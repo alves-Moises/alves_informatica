dc1caf9adea2297cb52f1a10ffcb8dfd42ac8297
alves-Moises <moisesinho_ra@hotmail.com>
2026-01-07 17:43:31 -0300

    update navbar link adress

--- template-parts/navigation/navbar.php
+++ template-parts/navigation/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-nav text-uppercase fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="#page-top">Alves Informática</a>
+        <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">Alves Informática</a>
         <button class="navbar-toggler text-uppercase font-weight-bold text-white b-menu rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
