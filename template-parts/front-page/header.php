29ebacefdd5f574833627e0aa9c040e500ecf243
alves-Moises <moisesinho_ra@hotmail.com>
2026-01-07 17:43:06 -0300

    update header link

--- template-parts/front-page/header.php
+++ template-parts/front-page/header.php
@@ -4,15 +4,21 @@
     style="background-image: url('<?= THEMEROOT ?>/assets/img/background/banner.jpg')"
 >
     <div class="container d-flex align-items-center flex-column">
-        <h1 class="masthead-heading text-uppercase mb-0">Alves Informática</h1>
+        <h1 class="masthead-heading text-uppercase mb-0">
+            Alves Informática
+        </h1>
 
         <!-- Icon Divider-->
         <div class="divider-custom divider-light">
             <div class="divider-custom-line"></div>
-            <div class="divider-custom-icon"><i class="fa-solid fa-microchip"></i></div>
+            <div class="divider-custom-icon">
+                <i class="fa-solid fa-microchip"></i>
+            </div>
             <div class="divider-custom-line"></div>
         </div>
         <!-- Masthead Subheading-->
-        <p class="masthead-subheading font-weight-light mb-0">Informática - Manutenção de smartphone - Programador</p>
+        <p class="masthead-subheading font-weight-light mb-0">
+            Informática - Manutenção de smartphone - Programador
+        </p>
     </div>
 </header>
