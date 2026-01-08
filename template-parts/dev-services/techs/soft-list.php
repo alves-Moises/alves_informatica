192c096399e5bd8ed7d666e786f94fcc3a1929b2
alves-Moises <moisesinho_ra@hotmail.com>
2026-01-08 05:20:21 -0300

    new tech tempalte parts

---
+++ template-parts/dev-services/techs/soft-list.php
@@ -0,0 +1,5 @@
+<div class="list-icon-tech">
+
+
+
+</div>
---
+++ template-parts/dev-services/techs/tech-list.php
@@ -0,0 +1,39 @@
+<div 
+    class="card card-tech"
+>
+    <div class="card-body">
+        <i 
+            class="fa-solid fa-language fa-lg tech-i mb-3" 
+        ></i>
+        <h3 class="card-title">Linguagens</h3>
+        <?php
+            get_template_part("template-parts/dev-services/techs/lang-list");
+        ?>
+    </div>
+</div>
+
+<div class="card card-tech">
+    <div class="card-body">
+        <i class="fa-solid fa-gears fa-lg tech-i mb-3"></i>
+
+
+        <h3 class="card-title">Frameworks</h3>
+        <?php
+            get_template_part("template-parts/dev-services/techs/framk-list");
+        ?>
+    </div>
+</div>
+
+<div class="card card-tech">
+    <div class="card-body">
+        <i class="fa-solid fa-gears fa-lg tech-i mb-3"></i>
+
+
+        <h3 class="card-title">Softwares</h3>
+        <?php
+            get_template_part("template-parts/dev-services/techs/soft-list");
+        ?>
+    </div>
+
+
+</div>
