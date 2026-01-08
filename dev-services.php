f4fa06cb414e4a547ffc91d8792969fc1e8537b0
alves-Moises <moisesinho_ra@hotmail.com>
2026-01-07 22:57:04 -0300

    new: dev page

--- dev-services.php
+++ dev-services.php
@@ -5,5 +5,10 @@
 ?>
 
 <?php get_header(); ?>
+<?php get_template_part("template-parts/dev-services/navbar"); ?>
+<?php get_template_part("template-parts/dev-services/who-am-i"); ?>
+<?php get_template_part("template-parts/dev-services/techs"); ?>
+<?php get_template_part("template-parts/dev-services/projects"); ?>
+<?php get_template_part("template-parts/dev-services/contact"); ?>
 
 <?php get_footer(); ?>
