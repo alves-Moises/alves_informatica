e569db9bb74c98d9478397d79b3a3cb67d95998d
alves-Moises <moisesinho_ra@hotmail.com>
2026-01-07 22:58:50 -0300

    update section who-am-i

--- template-parts/dev-services/who-am-i.php
+++ template-parts/dev-services/who-am-i.php
@@ -1,3 +1,35 @@
-<section>
-    <h1>WhoAmI</h1>
+<section 
+    id="who-am-i"
+>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 pt-3 pr-5 text-justify">
+                <h1 class="pl-3 mb-4">
+                    Quem sou eu
+                </h1>
+
+                <p>
+                    Olá, meu nome é Moisés, estudante de Tecnologia em Sistemas Para computação. 
+                    Possuo experiência em diversas áreas de TI como desenvolvimento, gestão e suporte técnico. 
+                    Estagiei em uma agência de marketing onde foi minha primeira experiência profissional em equipe. 
+                    Também atuei com startups, atuando em diferentes stacks como gestão e desenvolvimento.
+                </p>
+
+                <p>
+                    Mas não é só sobre código e tecnologia... 
+                    Sou apaixonado por música e toco diversos instrumentos, como violão, guitarra, baixo e bateria. 
+                    Estou sempre envolvido com projetos culturais e atuo como músico há cerca de 10 anos. 
+                    Acredito que a criatividade e a inovação são fundamentais para o sucesso em qualquer área.
+                </p>
+            </div>
+
+            <div class="col-lg-5">
+                <img 
+                    id="sprite"
+                    src="<?= THEMEROOT ?>/assets/img/photos/me.jpg"
+                />
+            </div>
+        </div>
+    </div>
+
 </section>
