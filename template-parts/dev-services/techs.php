669a7eb1ac85d2b7f0d82177bd1e9a08f4d1fb7d
alves-Moises <moisesinho_ra@hotmail.com>
2026-01-08 05:19:02 -0300

    new: tech template part

--- template-parts/dev-services/techs.php
+++ template-parts/dev-services/techs.php
@@ -1,3 +1,21 @@
-<section>
-    <h1>tecyhs</h1>
+<section
+    id="techs"
+>
+    <div class="container">
+        <div class="row">
+            <h1 class="text-center">Techs</h1>
+
+            <!-- Icon Divider-->
+            <div class="divider-custom">
+                <div class="divider-custom-line i-symbols-line"></div>
+                <div class="divider-custom-icon"><i class="fa-solid fa-code" style="color: #ffffff;"></i></div>
+                <div class="divider-custom-line i-symbols-line"></div>
+            </div>
+
+            <div id="tech-container">
+                <?php get_template_part("template-parts/dev-services/techs/tech-list"); ?>
+
+            </div>
+        </div>
+    </div>
 </section>
