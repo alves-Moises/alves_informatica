6ce9e6a91f135e685d672fe0c15f9c97c34c9b56
alves-Moises <moisesinho_ra@hotmail.com>
2026-01-10 07:52:46 -0300

    update projects section

--- template-parts/dev-services/projects.php
+++ template-parts/dev-services/projects.php
@@ -1,3 +1,30 @@
-<section>
-    <h1>Projects</h1>
+
+<section 
+    id="projects"
+>
+    <div class="container">
+        <div class="row text-center">
+            <h1>Projetos</h1>
+
+            <!-- Icon Divider-->
+            <div class="divider-custom mt-2 mb-5">
+                <div class="divider-custom-line i-symbols-line"></div>
+                <div class="divider-custom-icon">
+                    <i 
+                        class="fa-solid fa-microchip" 
+                        style="color: #ffffff;"
+                    >
+                    </i>
+                </div>
+                <div class="divider-custom-line i-symbols-line"></div>
+            </div>
+
+            <?php 
+                get_template_part(
+                    "template-parts/dev-services/projects/project-container",
+                ); 
+            ?>
+            
+        </div>
+    </div>
 </section>
