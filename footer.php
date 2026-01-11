52d222d9b43a80b96a9e1e37f6e2c08e41e2da04
alves-Moises <moisesinho_ra@hotmail.com>
2026-01-11 16:23:20 -0300

    refactor identification

--- footer.php
+++ footer.php
@@ -18,7 +18,8 @@
 				<h4 class="text-uppercase mb-4 footer-title">Contato</h4>
 
 				<a 
-					class="btn btn-outline-light btn-social mx-1 btn-wpp-footer" 
+					class="btn btn-outline-light btn-social mx-1" 
+					id="btn-wpp-footer" 
 					href="https://wa.me/5522999668509?text=Olá,+encontrei+sua+empresa+pelo+seu+site!+Gostaria+de+fazer+um+orçamento+:)"
 					target="_blank"
 				>
