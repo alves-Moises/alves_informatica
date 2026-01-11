f197583e8d375cae0a1a5697b6fb1f22151b8a45
alves-Moises <moisesinho_ra@hotmail.com>
2026-01-11 03:03:09 -0300

    update contact page

--- template-parts/dev-services/contact.php
+++ template-parts/dev-services/contact.php
@@ -1,3 +1,63 @@
-<section>
-    <h1>Contact</h1>
+<section id="contact">
+    <div class="container">
+        <div class="row">
+            <h1 class="text-center">Entre em contato!</h1>
+
+            <div class="contact-subtitle">
+                <h6 class="text-justify">
+                    Gostaria de solicitar um orçamento sem compromisso?
+                </h6>
+                
+                <h6 class="text-justify">
+                    Entre em contato! Estamos ansiosos para atendê-lo
+                </h6>
+            </div>
+
+            <div class="separator">
+                <a 
+                    class="contact-link"
+                    id="email-link"
+                    href="mailto:moisesinho_ra@hotmail.com?subject=Servicos+de+TI"
+                    target="_blank"
+                >
+                    <span>
+                        Email
+                    </span>    
+                    <i class="fa-regular fa-envelope contact-i">
+                    </i>
+                </a>
+
+                <a 
+                    class="contact-link"
+                    id="linkedin-link"
+                    href="https://www.linkedin.com/in/alves-moises/"
+                    target="_blank"
+                >
+                    <span>
+                        Linkedin
+                    </span>    
+                    <i class="fa-brands fa-linkedin contact-i">
+
+                    </i>
+                    </i>
+                </a>
+
+                <a 
+                    class="contact-link"
+                    id="wpp-link"
+                    href="
+                        https://wa.me/22999668509?txt=Ola+gostaria+de+fazer+um+orçamento
+                    "
+                    target="_blank"
+                >
+                    <span>
+                        Whatsapp
+                    </span>
+                    <i class="fa-brands fa-whatsapp contact-i">
+                    </i>
+                </a>
+
+            </div>
+        </div>
+    </div>
 </section>
