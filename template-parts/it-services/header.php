fb7117fa4f47a04f93e4a0ba6ca6d636c0cb762e
alves-Moises <moisesinho_ra@hotmail.com>
2026-01-16 01:12:17 -0300

    update it-services header

--- template-parts/it-services/header.php
+++ template-parts/it-services/header.php
@@ -1,4 +1,24 @@
-<header>
+<header id="it-header">
+    <div class="container">
+        <div class="row">
+            <div
+                class="col-lg-6"
+            >
+                <h3>A solução para o seu negócio!</h3>
+                <p>Oferecemos soluções de Ti para diversos segmentos</p>
 
-    <h2 class="text-center">Header</h2>
+                <a 
+                    href=""
+                    id="a-scroll"
+                >Ver mais</a>
+            </div>
+
+            <dv 
+                class="col-lg-6"
+                id="it-services-rside"    
+            >
+                <img src="<?= THEMEROOT ?>/assets/img/it-services/notebook.jpg" alt="">
+            </dv>
+        </div>
+    </div>
 </header>
