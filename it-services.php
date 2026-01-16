8063d1087fc52b0e0ab1e3ae2a082f7da0265c9c
alves-Moises <moisesinho_ra@hotmail.com>
2026-01-16 01:10:36 -0300

    update it services page

--- it-services.php
+++ it-services.php
@@ -7,7 +7,8 @@
 
 <?php 
     get_header();
-    // get_template_part("template-parts/it-services/header");
+    get_template_part("template-parts/it-services/navbar");
+    get_template_part("template-parts/it-services/header");
     get_template_part("template-parts/it-services/services");
     get_template_part("template-parts/it-services/why-to-hire");
 
